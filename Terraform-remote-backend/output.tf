1b48e1fccb3258fa62da20c977bec6b3130bf273
Serge <pagnolfeuyan@yahoo.com>
2024-11-20 22:43:30 -0500

    ticket number

---
+++ Terraform-remote-backend/output.tf
@@ -0,0 +1,12 @@
+output "ip-private" {
+value =   aws_lightsail_instance.server1.private_ip_address
+
+}
+
+output "ip-public" {
+ value = aws_lightsail_instance.server1.public_ip_address 
+}
+
+output "username" {
+value = aws_lightsail_instance.server1.username  
+}
---
+++ code2/Output.tf
@@ -0,0 +1,7 @@
+output "my-ip" {
+value = aws_lightsail_instance.l1.public_ip_address  
+}
+
+output "my-username" {
+    value = aws_lightsail_instance.l1.username
+}
